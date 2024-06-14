a969348384d77fa850d971dd470b687d32f327f7
unknown <muthukumar24081999@gmail.com>
2024-06-14 23:28:49 +0530

    first commit

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ 
+  origin: 'https://demo-password-reset.netlify.app',
+  credentials: true 
+}));
+
 app.use(bodyParser.json());
 
 // Routes
