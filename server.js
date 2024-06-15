7c776b382b01428c55b19ce2d4f2d952fb58c420
Muthu kumar <muthukumar24081999@gmail.com>
2024-06-15 11:54:26 +0530

    first commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({ 
-  origin: 'https://demo-password-reset.netlify.app',
+  origin: 'https://demo-password-reset.netlify.app/',
   credentials: true 
 }));
 
